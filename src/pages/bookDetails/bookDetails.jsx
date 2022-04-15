a17d6dad2c43b3c62b52abceb0b0721904926952
fengjiqiang <fengjiqiang@boom.cn>
2022-04-15 18:13:55 +0800

    feat: 详情里书加上标签

--- src/pages/bookDetails/bookDetails.jsx
+++ src/pages/bookDetails/bookDetails.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
-import { View, Image } from '@tarojs/components'
+import { ScrollView, View, Image } from '@tarojs/components'
+import { AtTag } from 'taro-ui'
 
 
 export default class BookDetails extends Component {
@@ -31,26 +32,41 @@ export default class BookDetails extends Component {
     }
 
     renderBookInfo = () => {
-        const { title, pubdate, author, introduction, publisher, translator, image } = this.state.bookInfo
+        const { title, pubdate, author, introduction, publisher, translator, image, tags } = this.state.bookInfo
         return (
-            <View className='at-article'>
-                <View className='at-article__h1'>{title}</View>
-                <View className='at-article__info'>
-                    {translator}【译】&nbsp;&nbsp;&nbsp;{publisher}
-                </View>
-                <View className='at-article__content'>
-                    <View className='at-article__section'>
-                        <View className='at-article__h2'>{author}【著】</View>
-                        <View className='at-article__h3'>{pubdate}【出版】</View>
-                        <View className='at-article__p'>{introduction}</View>
-                        <Image
-                          className='at-article__img'
-                          src={image}
-                          mode='widthFix'
-                        />
+            <ScrollView>
+                <View className='at-article'>
+                    <View className='at-article__h1'>{title}</View>
+                    <View className='at-article__info'>
+                        {translator}【译】&nbsp;&nbsp;&nbsp;{publisher}
+                    </View>
+                    <View className='at-article__info' style={{ display: 'flex', flexDirection: 'row', marginTop: 10 }}>
+                        {
+                            tags?.map((item, index) => {
+                                return (
+                                    <View key={index.toString()} style={{ marginRight: 5 }}>
+                                        <AtTag type='primary' circle active>
+                                            {item}
+                                        </AtTag>
+                                    </View>
+                                )
+                            })
+                        }
+                    </View>
+                    <View className='at-article__content'>
+                        <View className='at-article__section'>
+                            <View className='at-article__h2'>{author}【著】</View>
+                            <View className='at-article__h3'>{pubdate}【出版】</View>
+                            <View className='at-article__p'>{introduction}</View>
+                            <Image
+                              className='at-article__img'
+                              src={image}
+                              mode='widthFix'
+                            />
+                        </View>
                     </View>
                 </View>
-            </View>
+            </ScrollView>
         )
     }
 
